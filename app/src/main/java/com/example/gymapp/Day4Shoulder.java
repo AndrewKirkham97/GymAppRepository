02f078d88879f864287c131a2f3e2a2a552d2663
Andrew Kirkham <andrewkirkham100@yahoo.com>
2023-08-20 17:17:37 -0500

    Adding Day4, better charts, Day 3 week 2 legs start

--- app/src/main/java/com/example/gymapp/Day4Shoulder.java
+++ app/src/main/java/com/example/gymapp/Day4Shoulder.java
@@ -2,13 +2,28 @@ package com.example.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Day4Shoulder extends AppCompatActivity {
+    private Button buttonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day4_shoulder);
+        buttonHome = (Button) findViewById(R.id.buttonHome);
+        buttonHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
     }
+    public void openHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    };
 }
